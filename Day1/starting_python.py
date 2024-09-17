e082399bd7c7ec552ad35a76c060a68e7f904800
Deewakar2002Jha <deewakarjha0541@gmail.com>
2024-09-17 23:54:23 +0530

    day one learning python form basic

---
+++ Day1/starting_python.py
@@ -0,0 +1,29 @@
+name = "deewakar jha"
+print(name)
+age = 39
+print(age)
+#defining the rule of variable in python
+#it cannot  be start with the number and use a keyword in python
+# 1234 or test@ or test! or if or else
+
+
+#know we learn abbout the datatype and we can check the type of the variable by type function
+print(type(name))
+print(type(name) == str)#we can check our datatype by using the this that the datatype difine actull hold the same datatyle
+
+#to check the instance of the string we use this
+print(isinstance(name, str))
+
+age = 2
+print(isinstance(age,float))
+age1 = 2.9
+print(isinstance(age1,float))
+#how can we change the datatype of variable in python using class constructer
+age3 = float(2)
+print(isinstance(age3,float))
+
+#now we can also pass a varible in datatype in varible by defining it in start
+number = "20"
+age = int(number)
+print(isinstance(age,float))#we can check it by using differnt datatype
+
