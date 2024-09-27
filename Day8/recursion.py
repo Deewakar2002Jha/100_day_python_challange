a9535ebac6f1ee705f897d0dfc9ee7c00455f0ad
Deewakar2002Jha <deewakarjha0541@gmail.com>
2024-09-27 23:43:29 +0530

    Recursion

--- Day8/recursion.py
+++ Day8/recursion.py
@@ -101,8 +101,8 @@ try:
 finally:
     file.close()
 """with with statement"""
- filename= '/Users/flavio/test.txt'
+filename= '/Users/flavio/test.txt'
 
- with open(filename, 'r') as file:
-     content = file.read()
-     print(content)
+with open(filename, 'r') as file:
+    content = file.read()
+    print(content)
